fe469d748e406a23c56ea11b8531e8eccbfb80a1
Asmaa Badreldin <asmaabdreldin@gmail.com>
2025-04-12 13:01:57 +0200

    initial product setup

---
+++ MazaadyApp/MazaadyAppTests/MazaadyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MazaadyAppTests.swift
+//  MazaadyAppTests
+//
+//  Created by asmaa badreldin on 12/04/2025.
+//
+
+import Testing
+@testable import MazaadyApp
+
+struct MazaadyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
